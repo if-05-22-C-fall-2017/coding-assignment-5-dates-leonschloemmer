790253f392db641884846c0ca911b276cff491cc
leon.schloemmer <leon.schloemmer@gmail.com>
2018-04-26 10:00:00 +0200

    completed Homework, made all test cases work.

--- src/main/java/GermanStyleDateChecker.java
+++ src/main/java/GermanStyleDateChecker.java
@@ -3,6 +3,7 @@ import java.util.regex.Pattern;
 
 public class GermanStyleDateChecker {
     public static Matcher getMatcher(String dateString) {
-        return null;
+        Pattern p = Pattern.compile("^(([0-2]?[0-9])|(30|31))[\\.\\-\\s]((0?[1-9])|(1[0-2]))[\\.\\-\\s]((19|20)?[0-9]{2})$");
+        return p.matcher(dateString);
     }
 }
